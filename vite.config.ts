6e22cd8568282d61cbe4854226baae53eddc5de0
Mo-101 <akiniobong10@gmail.com>
2025-06-23 08:39:36 +0300

    OK

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
     server: {
@@ -26,6 +24,7 @@ export default defineConfig(({ mode }) => {
     },
     define: {
       global: 'globalThis',
+      __WS_TOKEN__: JSON.stringify(''),
     },
   };
 });
